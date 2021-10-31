2bc7b67d19a812760f29c0da39df7b377951cdd3
Hamza Sehouli <sehouli.hamza@gmail.com>
2021-10-31 14:53:05 +0100

    Fix the error in uploading image, return if there is no image input and the user tries to press the save button

--- public/scripts/uploadImage.js
+++ public/scripts/uploadImage.js
@@ -1,17 +1,18 @@
-const uploadImageBtn = document.querySelector('.upload-image');
-// const uploadImageInput = document.querySelector('.upload-image__input');
+const uploadImageForm = document.querySelector('.upload-image');
+const uploadImageInput = document.querySelector('.upload-image__input');
 
-export default uploadImageBtn?.addEventListener('submit', async (e) => {
+export default uploadImageForm?.addEventListener('submit', async (e) => {
   e.preventDefault();
-  // This assumes the form's name is `myForm`
+
+  if (!uploadImageInput.value) return;
+
   const form = document.querySelector('.upload-image');
   const formData = new FormData(form);
   const res = await fetch('http://127.0.0.1:3000/api/v1/users/upload-image', {
     method: 'POST',
     body: formData,
   });
-  console.log('äää');
-  console.log(res);
+
   if (res.ok) {
     window.location.replace('/profile');
   }
